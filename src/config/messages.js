2968e1f61af0432cf8d7fab0ae1538809f34c2c3
Marc Studzinski <marc.studzinski@web.de>
2023-08-23 14:37:17 +0200

    Refactoring

---
+++ src/config/messages.js
@@ -0,0 +1,44 @@
+module.exports = {
+	gdkpraids: {
+		successTitle: 'GDKP Raid Sign Ups',
+		successMessage: '',
+		errorTitle: 'Missing Infos',
+		errorMessage: ''
+	},
+	mysetups: {
+		successTitle: 'Setups',
+		successMessage: '',
+		errorTitle: 'Setups',
+		errorMessage: 'Momentan in keinem Setup gesetzt. Neue Setups kommen bald!',
+
+	},
+	lastspent: {
+		successTitle: 'Letzte ID gekauft: ',
+		successMessage: '',
+		errorTitle: 'Letzte ID gekauft: ',
+		errorMessage: 'Keine Items gekauft in der letzten ID. Eventuell ist die Datenbank nicht aktuell!'
+	},
+	currentspent: {
+		successTitle: 'Diese ID gekauft: ',
+		successMessage: '',
+		errorTitle: 'Diese ID gekauft: ',
+		errorMessage: 'Keine Items gekauft in der momentanen ID. Eventuell ist die Datenbank nicht aktuell!'
+	},
+	totalspent: {
+		successTitle: 'Bisher im GDKP gekauft: ',
+		successMessage: '',
+		errorTitle: 'Bisher im GDKP gekauft: ',
+		errorMessage: 'Keine Items gekauft. Eventuell ist die Datenbank nicht aktuell!'
+	},
+	signup: {
+		successTitle: 'Sign Up',
+		successMessage: '',
+		errorTitle: 'Anmeldung nicht möglich',
+		errorMessage: 'Keinen passenden Raid gefunden.'
+	},
+	common: {
+		pulseBotSetupError: 'Pulse Bot doesnt have a correct Setup yet.',
+		pulseBotReady: 'Pulse Bot is ready!'
+	}
+}
+
