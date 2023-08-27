173b3788fd3c7dd7fe08ce4ae150dc3848400b19
mst1987 <marc.studzinski@web.de>
2023-08-27 18:16:17 +0200

    Legendary Updates

--- src/classes/legendary.js
+++ src/classes/legendary.js
@@ -1,37 +1,17 @@
-const https = require('https');
+const axios = require('axios');
 class Legendary {
     constructor() {}
 
     async createAuction(auctionData) {
-        return new Promise(async(resolve, reject) => {
-            const options = {
-                host: "https://pulse-gdkp.de:3001",
-                port: 443,
-                path: "/api/legendary/createauction",
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Content-Length': auctionData.length
-                }
-            };
-
-            const request = await https.request(options, (response) => {
-                let data = '';
-                response.on('data', (chunk) => {
-                    data += chunk;
+        const url = "https://pulse-gdkp.de:3001/api/legendary/createauction";
+        return new Promise((resolve, reject) => {
+            axios.post(url, auctionData)
+                .then(response => {
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    reject(error);
                 });
-
-                response.on('end', () => {
-                    resolve(data); // Resolve the promise with the response data
-                });
-            });
-
-            request.on('error', (error) => {
-                reject(error); // Reject the promise if there's an error
-            });
-
-            request.write(auctionData);
-            request.end();
         });
     }
 
