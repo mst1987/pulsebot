0e86e13ee881843fd863db5f7929c0a03bffd1b8
mst1987 <marc.studzinski@web.de>
2023-08-27 23:08:52 +0200

    Legendary Updates

--- src/classes/legendary.js
+++ src/classes/legendary.js
@@ -5,7 +5,7 @@ class Legendary {
     async createAuction(auctionData) {
         return new Promise(async(resolve, reject) => {
             const options = {
-                host: "https://pulse-gdkp.de",
+                host: "https://www.pulse-gdkp.de",
                 port: 3001,
                 path: "/api/legendary/createauction",
                 method: "POST",
