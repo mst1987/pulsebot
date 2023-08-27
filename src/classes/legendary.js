79723884647ccd999ed1ff24a60f8baa02b20d3b
mst1987 <marc.studzinski@web.de>
2023-08-28 00:19:05 +0200

    Legendary Updates

--- src/classes/legendary.js
+++ src/classes/legendary.js
@@ -17,7 +17,7 @@ class Legendary {
                 return response.data;
             }).catch((error) => {
                 console.log('Error: ', error.message)
-                return error.data;
+                return error;
             })
 
             resolve(res);
