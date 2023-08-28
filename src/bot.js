9f9e5fb3e4c7d5b56e47511f8d305afcd79b88a1
mst1987 <marc.studzinski@web.de>
2023-08-28 10:53:18 +0200

    Legendary Updates

--- src/bot.js
+++ src/bot.js
@@ -236,7 +236,7 @@ client.on('interactionCreate', async(interaction) => {
         if (response.type === 'success') {
             botReply(interaction, 'Auktion gelöscht', response.message);
         } else {
-            console.log(response)
+            console.log(response.response)
             botReply(interaction, 'Fehler', 'Ein Fehler ist vorgefallen...');
         }
     }
