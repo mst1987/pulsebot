6b1345619abc08ef52b3413135fe226485adf3db
mst1987 <marc.studzinski@web.de>
2023-08-31 09:33:10 +0200

    Update Helper Functions

--- src/bot.js
+++ src/bot.js
@@ -6,6 +6,7 @@ const GDKP = require('./classes/gdkp.js');
 const Legendary = require('./classes/legendary.js');
 const messages = require('./config/messages.js');
 
+const { botReply, botFollowup, findServerEmoji, formatNumberWithDots, formatSignUps, getAuctionMessage, getChannelsFromCategories, getCharacterIcon, getItemsToShow, getUserNickname, getWednesdayWeeksAgo, isNumber, toTimestamp } = require('./functions/helper');
 const { Client, GatewayIntentBits, MessageEmbed } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent], partials: ['MESSAGE', 'REACTION'] });
@@ -131,21 +132,14 @@ client.on('interactionCreate', async(interaction) => {
                 i++;
             })
             const sumOfGold = totalItems.reduce((totalGold, entry) => totalGold + entry.gold, 0);
-            await botReply(interaction, messages.totalspent.successTitle, `Gesamtausgaben: **${sumOfGold}g**\n\n${formattedItems[0].join('\n')}`);
 
-            if (formattedItems.length > 1)
-                formattedItems.forEach(async(items, key) => {
-                    if (key > 0)
-                        await interaction.followUp({
-                            embeds: [{
-                                description: `${items.join('\n')}`,
-                            }],
-                            ephemeral: true
-                        }).then(msg => {
-                            if (timeout > 0)
-                                setTimeout(() => msg.delete(), timeout)
-                        });
-                })
+            formattedItems.forEach(async(items, key) => {
+                if (key > 0) {
+                    await botReply(interaction, messages.totalspent.successTitle, `Gesamtausgaben: **${sumOfGold}g**\n\n${formattedItems[0].join('\n')}`);
+                } else {
+                    await botFollowup(interaction, items.join('\n'))
+                }
+            })
         }
     }
 
@@ -255,7 +249,7 @@ client.on('interactionCreate', async(interaction) => {
             return;
         }
 
-        await interaction.reply({
+        const replyMessage = await interaction.reply({
             embeds: [{
                 title: 'title',
                 description: 'message'
@@ -263,8 +257,6 @@ client.on('interactionCreate', async(interaction) => {
             ephemeral: false
         })
 
-        const replyMessage = await interaction.fetchReply()
-        console.log(replyMessage.id);
         const legendary = new Legendary();
         const auctionData = {
             name: interaction.options.getString('name'),
@@ -277,12 +269,14 @@ client.on('interactionCreate', async(interaction) => {
         }
 
         response = await legendary.createAuction(auctionData);
-        console.log(response)
+
         if (response.type === 'success') {
             const targetMessage = await interaction.channel.messages.fetch(replyMessage.id);
             const embed = { title: `${findServerEmoji('poggies')} Auktion gestartet ${findServerEmoji('poggies')}`, description: `Auktion wurde gestartet\n\n${getAuctionMessage(response.legendary)}` };
             const newMessage = await targetMessage.edit({ embeds: [embed] });
             await legendary.updateAuction({ messageid: newMessage.id })
+        } else {
+            botReply(interaction, 'Fehler', 'Ein Fehler ist vorgefallen...');
         }
     }
 
@@ -360,138 +354,5 @@ client.on('interactionCreate', async(interaction) => {
     // --------------------------------------------------------
 
 })
-
-function getAuctionMessage(legendary) {
-    return `${findServerEmoji('shadowmourne')}  **${legendary.name}**\n\nRaid: **${legendary.raid}**\nAuktion endet am **${formatTimestampToDateString(Number(legendary.endtime))}**\n\nStartpreis ist **${legendary.mingold}g** und Mindesterhöhung liegt bei **${legendary.increment}g**\n\nBenutze den /bid Befehl um mitzubieten!`
-}
-
-function isNumber(value) {
-    return typeof value === 'number' && !isNaN(value);
-}
-
-// Function to parse "D-M-YYYY" format
-function parseDMYDateString(dateString) {
-    const parts = dateString.split('-');
-    const day = parseInt(parts[0], 10);
-    const month = parseInt(parts[1], 10) - 1; // Months in JavaScript are zero-based
-    const year = parseInt(parts[2], 10);
-    return new Date(year, month, day);
-}
-
-async function getUserNickname(interaction) {
-    const displayName = await interaction.guild.members.fetch(interaction.user.id);
-    return displayName;
-}
-
-function getWednesdayWeeksAgo(weeks) {
-    const now = new Date();
-    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-    // Calculate the number of days to subtract to get to the previous Wednesday
-    const daysToSubtract = ((today.getDay() + 4) % 7) + 7 * (weeks - 1);
-
-    // Subtract two weeks' worth of days and the calculated daysToSubtract
-    const weeksAgo = new Date(today.getTime() - daysToSubtract * 24 * 60 * 60 * 1000);
-
-    return weeksAgo;
-}
-
-async function botReply(interaction, title, message, timeout = timeoutTime, ephemeral = true) {
-    await interaction.reply({
-            embeds: [{
-                title: title,
-                description: message
-            }],
-            ephemeral: ephemeral
-        }).then(msg => {
-            if (timeout > 0)
-                setTimeout(() => msg.delete(), timeout)
-        })
-        .catch(error => {
-            console.log(error);
-        });
-}
-
-function getItemsToShow(items, dateFrom, dateEnd) {
-    const filteredItems = items.filter((entry) => {
-        const entryDate = parseDMYDateString(entry.date);
-        return entryDate >= dateFrom && entryDate <= dateEnd;
-    }).sort((a, b) => a.player.localeCompare(b.player));
-
-    const formattedItems = getItemsFormatted(filteredItems);
-    const sumOfGold = filteredItems.reduce((totalGold, entry) => totalGold + entry.gold, 0);
-    return `${formattedItems}\n\n\nGesamtausgaben: **${sumOfGold}g**`;
-}
-
-function formatSignUps(specs) {
-    let signUps = [];
-    if (specs) {
-        specs = specs.split(',')
-        specs = specs.slice(0, 10)
-        specs.forEach(spec => {
-            if (extendedClassList[spec])
-                signUps.push({ className: extendedClassList[spec].clazz, specName: extendedClassList[spec].spec })
-        })
-    }
-
-    return signUps;
-}
-
-function getChannelsFromCategories(categoryIds) {
-    const channelsFromCategories = [];
-
-    guild.channels.cache.forEach(channel => {
-        if (channel.type === 0) {
-            const parent = channel.parent;
-            if (parent && categoryIds.includes(parent.id)) {
-                channelsFromCategories.push(channel.id);
-            }
-        }
-    });
-
-    return channelsFromCategories;
-}
-
-function getCharacterIcon(spec) {
-    return `${guild.emojis.cache.find(emoji => emoji.name === extendedClassList[spec]?.icon)}`;
-}
-
-function findServerEmoji(emojiName) {
-    return `${guild.emojis.cache.find(emoji => emoji.name === emojiName)}`;
-}
-
-function getItemsFormatted(items) {
-    return items.map(item => `${getCharacterIcon(item.class)} ${item.player} - [${item.item}](${item.wowhead}) - ${item.gold}g`).join(`\n`);
-}
-
-function toTimestamp(dateString) {
-    const [datePart, timePart] = dateString.split('-');
-    const [day, month, year] = datePart.split('.').map(Number);
-    const [hour, minute] = timePart.split(':').map(Number);
-
-    // Note: In the following line, the year is assumed to be in the 21st century (20xx)
-    const dateObject = new Date(2000 + year, month - 1, day, hour, minute);
-
-    const timestamp = dateObject.getTime(); // Convert milliseconds to seconds
-
-    return timestamp
-}
-
-function formatTimestampToDateString(timestamp) {
-    const dateObject = new Date(timestamp); // Convert seconds to milliseconds
-
-    const day = String(dateObject.getDate()).padStart(2, '0');
-    const month = String(dateObject.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-    const year = dateObject.getFullYear();
-    const hour = String(dateObject.getHours()).padStart(2, '0');
-    const minute = String(dateObject.getMinutes()).padStart(2, '0');
-
-    const formattedDateString = `${day}.${month}.${year} um ${hour}:${minute} Uhr`;
-    return formattedDateString;
-}
-
-function formatNumberWithDots(number) {
-    const formattedNumber = number.toLocaleString('en-US'); // Use the 'en-US' locale for comma (,) separator
-    return formattedNumber.replace(/,/g, '.');
-}
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
---
+++ src/functions/helper.js
@@ -0,0 +1,147 @@
+export function getAuctionMessage(legendary) {
+    return `${findServerEmoji('shadowmourne')}  **${legendary.name}**\n\nRaid: **${legendary.raid}**\nAuktion endet am **${formatTimestampToDateString(Number(legendary.endtime))}**\n\nStartpreis ist **${legendary.mingold}g** und Mindesterhöhung liegt bei **${legendary.increment}g**\n\nBenutze den /bid Befehl um mitzubieten!`
+}
+
+export function isNumber(value) {
+    return typeof value === 'number' && !isNaN(value);
+}
+
+// Function to parse "D-M-YYYY" format
+export function parseDMYDateString(dateString) {
+    const parts = dateString.split('-');
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1; // Months in JavaScript are zero-based
+    const year = parseInt(parts[2], 10);
+    return new Date(year, month, day);
+}
+
+export async function getUserNickname(interaction) {
+    const displayName = await interaction.guild.members.fetch(interaction.user.id);
+    return displayName;
+}
+
+export function getWednesdayWeeksAgo(weeks) {
+    const now = new Date();
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    // Calculate the number of days to subtract to get to the previous Wednesday
+    const daysToSubtract = ((today.getDay() + 4) % 7) + 7 * (weeks - 1);
+
+    // Subtract two weeks' worth of days and the calculated daysToSubtract
+    const weeksAgo = new Date(today.getTime() - daysToSubtract * 24 * 60 * 60 * 1000);
+
+    return weeksAgo;
+}
+
+export async function botReply(interaction, title, message, timeout = timeoutTime, ephemeral = true) {
+    await interaction.reply({
+            embeds: [{
+                title: title,
+                description: message
+            }],
+            ephemeral: ephemeral
+        }).then(msg => {
+            if (timeout > 0)
+                setTimeout(() => msg.delete(), timeout)
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+export async function botFollowup(interaction, message, timeout = timeoutTime, ephemeral = true) {
+    await interaction.followUp({
+            embeds: [{
+                description: message
+            }],
+            ephemeral: ephemeral
+        }).then(msg => {
+            if (timeout > 0)
+                setTimeout(() => msg.delete(), timeout)
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+export function getItemsToShow(items, dateFrom, dateEnd) {
+    const filteredItems = items.filter((entry) => {
+        const entryDate = parseDMYDateString(entry.date);
+        return entryDate >= dateFrom && entryDate <= dateEnd;
+    }).sort((a, b) => a.player.localeCompare(b.player));
+
+    const formattedItems = getItemsFormatted(filteredItems);
+    const sumOfGold = filteredItems.reduce((totalGold, entry) => totalGold + entry.gold, 0);
+    return `${formattedItems}\n\n\nGesamtausgaben: **${sumOfGold}g**`;
+}
+
+export function formatSignUps(specs) {
+    let signUps = [];
+    if (specs) {
+        specs = specs.split(',')
+        specs = specs.slice(0, 10)
+        specs.forEach(spec => {
+            if (extendedClassList[spec])
+                signUps.push({ className: extendedClassList[spec].clazz, specName: extendedClassList[spec].spec })
+        })
+    }
+
+    return signUps;
+}
+
+export function getChannelsFromCategories(categoryIds) {
+    const channelsFromCategories = [];
+
+    guild.channels.cache.forEach(channel => {
+        if (channel.type === 0) {
+            const parent = channel.parent;
+            if (parent && categoryIds.includes(parent.id)) {
+                channelsFromCategories.push(channel.id);
+            }
+        }
+    });
+
+    return channelsFromCategories;
+}
+
+export function getCharacterIcon(spec) {
+    return `${guild.emojis.cache.find(emoji => emoji.name === extendedClassList[spec]?.icon)}`;
+}
+
+export function findServerEmoji(emojiName) {
+    return `${guild.emojis.cache.find(emoji => emoji.name === emojiName)}`;
+}
+
+export function getItemsFormatted(items) {
+    return items.map(item => `${getCharacterIcon(item.class)} ${item.player} - [${item.item}](${item.wowhead}) - ${item.gold}g`).join(`\n`);
+}
+
+export function toTimestamp(dateString) {
+    const [datePart, timePart] = dateString.split('-');
+    const [day, month, year] = datePart.split('.').map(Number);
+    const [hour, minute] = timePart.split(':').map(Number);
+
+    // Note: In the following line, the year is assumed to be in the 21st century (20xx)
+    const dateObject = new Date(2000 + year, month - 1, day, hour, minute);
+
+    const timestamp = dateObject.getTime(); // Convert milliseconds to seconds
+
+    return timestamp
+}
+
+export function formatTimestampToDateString(timestamp) {
+    const dateObject = new Date(timestamp); // Convert seconds to milliseconds
+
+    const day = String(dateObject.getDate()).padStart(2, '0');
+    const month = String(dateObject.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const year = dateObject.getFullYear();
+    const hour = String(dateObject.getHours()).padStart(2, '0');
+    const minute = String(dateObject.getMinutes()).padStart(2, '0');
+
+    const formattedDateString = `${day}.${month}.${year} um ${hour}:${minute} Uhr`;
+    return formattedDateString;
+}
+
+export function formatNumberWithDots(number) {
+    const formattedNumber = number.toLocaleString('en-US'); // Use the 'en-US' locale for comma (,) separator
+    return formattedNumber.replace(/,/g, '.');
+}
