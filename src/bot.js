b4c497d76ba5a73d09023d6d7891189edbef4256
Marc Studzinski <marc.studzinski@web.de>
2023-10-07 23:42:17 +0200

    Update Raidoverview

--- src/bot.js
+++ src/bot.js
@@ -76,6 +76,12 @@ client.on('interactionCreate', async(interaction) => {
                 // Format Signup and get Discord Emojis for the classes
                 const formattedSignUps = setupData.map(channel => `<#${channel.channelid}> ${getCharacterIcon(interaction, channel.setup[0].spec)} ${extendedClassList[channel.setup[0].spec].name}\n${formatTimestampToDateString(channel.startTime*1000)} Uhr\n`).join(`\n`);
 
+                const formattedNew = events.map(channel => 
+                    `<#${channel.channelid}> ${getCharacterIcon(interaction, channel.setup[0].spec)} ${extendedClassList[channel.setup[0].spec].name}\n${formatTimestampToDateString(channel.startTime*1000)} Uhr\n`)
+                    .join(`\n`);
+
+                console.log(formattedNew)
+
                 await botReply(interaction, messages.mysetups.successTitle, `\n${formattedSignUps}`)
             }   
         }
