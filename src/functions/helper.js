cf023bedb5f10df183e939e2c874abbfa6727bf8
mst1987 <marc.studzinski@web.de>
2023-10-09 12:16:58 +0200

    SaveRaid implementation

--- src/functions/helper.js
+++ src/functions/helper.js
@@ -94,13 +94,12 @@ async function getRaidInfosFromChannel(interaction) {
     const raidhelper = new Raidhelper();
     const channelMessages = await interaction.channel.messages.fetch();
     const botMessages = channelMessages.filter(msg => msg.author.id === '579155972115660803');
-    console.log(botMessages)
+
     for (const [key, value] of botMessages) {
         const event = await raidhelper.getEvent(key);
-        console.log(event)
+
         if (event.id) {
             const setup = await raidhelper.getSetup(event.id);
-            console.log(setup)
             return { raidData: createRaidData(event), setup: setup ? setup : [] };
         }
     }
