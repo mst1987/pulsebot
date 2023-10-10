f64d232d93a45f8f6e5e082fa48cfc329395baf3
mst1987 <marc.studzinski@web.de>
2023-10-10 22:48:20 +0200

    Change Highestbid handling

--- src/bot.js
+++ src/bot.js
@@ -67,7 +67,7 @@ client.on('interactionCreate', async(interaction) => {
                 await botEditReply(interaction, messages.mysetups.successTitle, `\n${mySetup}`)
             } catch (error) {
                 console.log('CATEGORYERROR:', error);
-                await botEditReply(interaction, messages.mysetups.errorTitle, messages.gdkpraids.errorMessage);
+                await botReply(interaction, messages.mysetups.errorTitle, messages.gdkpraids.errorMessage);
             }
         }
 
