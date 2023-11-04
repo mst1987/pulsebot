ace521e26fa50398a5ef382bc7ca8ba790be9646
Marc Studzinski <marc.studzinski@web.de>
2023-11-04 18:07:26 +0100

    Update Auction overview

--- src/functions/legendary.js
+++ src/functions/legendary.js
@@ -1,4 +1,4 @@
-async function updateHighestBids(interaction, targetMessage, legendary) {
+async function updateHighestBids(interaction, targetMessage, legendary, client) {
     const getHighestBids = await legendary.getHighestBids();
     const channel = await client.channels.fetch('1145659881362313248');
     if (channel) {
