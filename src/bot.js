bb3bac3c64e798c7026ccbeb73b382300cf6b51c
Marc Studzinski <marc.studzinski@web.de>
2023-11-04 18:09:39 +0100

    Update Auction overview

--- src/bot.js
+++ src/bot.js
@@ -300,6 +300,7 @@ client.on('interactionCreate', async(interaction) => {
                 botReply(interaction, `**${formatNumberWithDots(Number(bidData.gold))}g**`, `geboten von ${nickname}`, 0, false);
 
                 const targetMessage = getTargetMessage(client, highestBidsChannelId, highestBidsMessageId);
+                console.log(targetMessage)
                 if (targetMessage) {
                     updateHighestBids(interaction, targetMessage, legendary, client);
                 }
