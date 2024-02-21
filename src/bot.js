48d759afeb46f8c1814f6b0688e030975ac0702d
mst1987 <marc.studzinski@web.de>
2024-02-21 12:48:10 +0100

    SoD Updates

--- src/bot.js
+++ src/bot.js
@@ -75,7 +75,7 @@ client.on('interactionCreate', async(interaction) => {
             const setups = await getSetupsFromEvents(client, interaction, events);
             let mySetup;
             if (setups.length < 1) {
-                await botReply(interaction, messages.mysetups.errorTitle, messages.gdkpraids.errorMessage);
+                await botEditReply(interaction, messages.mysetups.errorTitle, messages.gdkpraids.errorMessage);
                 return;
             } else {
                 mySetup = setups.sort((eventA, eventB) => eventA.startTime - eventB.startTime).map((event) => {
@@ -83,7 +83,7 @@ client.on('interactionCreate', async(interaction) => {
                 }).join(`\n`)
             }
 
-            await botReply(interaction, 'Alle deine Setups auf dem Discord', `${mySetup}\n`)
+            await botEditReply(interaction, 'Alle deine Setups auf dem Discord', `${mySetup}\n`)
             return;
         }
     }
