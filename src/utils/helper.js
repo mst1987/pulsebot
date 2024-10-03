dca0c18dba6db655e4181820dd7eab24baf15512
mst1987 <marc.studzinski@web.de>
2024-10-03 17:07:10 +0200

    refactor v2

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -9,6 +9,7 @@ function isNumber(value) {
 }
 
 function getCharacterIcon(interaction, spec) {
+    console.log("spec", spec);
     return `${interaction.guild.emojis.cache.find(
     (emoji) => emoji.name === extendedClassList[spec]?.icon
   )}`;
