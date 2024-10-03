3fdca7cef94abb06ba8f57263489687ac6c169d7
mst1987 <marc.studzinski@web.de>
2024-10-03 18:03:53 +0200

    refactor v2

---
+++ src/commands/auction/bid5k.js
@@ -0,0 +1,9 @@
+const { showConfirmationModal, showBidModal } = require("../../utils/auction");
+
+module.exports = {
+    name: "bid-5k",
+    description: "Place a bid in an auction",
+    async execute(interaction, client) {
+        console.log("bid 5k");
+    },
+};
