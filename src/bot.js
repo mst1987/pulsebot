7e21b53a1eda13551771feecc05b9d474d8e8a05
mst1987 <marc.studzinski@web.de>
2024-10-03 16:03:13 +0200

    refactor v2

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,7 @@
 require("dotenv").config({ path: "../.env" });
 const messages = require("./config/messages.js");
 const fs = require("fs");
+const path = require("path");
 
 const { Client, GatewayIntentBits, Collection } = require("discord.js");
 
@@ -13,12 +14,7 @@ const client = new Client({
     partials: ["MESSAGE", "REACTION"],
 });
 
-// Globales Objekt zur Speicherung von Geboten
 const pendingBids = new Map();
-
-client.on("ready", () => {
-    console.log(messages.common.pulseBotReady);
-});
 
 client.commands = new Collection();
 
@@ -26,15 +22,40 @@ function loadCommands(dir) {
     const commandFolders = fs.readdirSync(dir);
     for (const folder of commandFolders) {
         const commandFiles = fs
-            .readdirSync(`${dir}/${folder}`)
+            .readdirSync(path.join(dir, folder))
             .filter((file) => file.endsWith(".js"));
         for (const file of commandFiles) {
-            const command = require(`${dir}/${folder}/${file}`);
+            const command = require(path.join(dir, folder, file));
             client.commands.set(command.name, command);
         }
     }
 }
 
-loadCommands("./commands");
+client.on("ready", () => {
+    console.log(messages.common.pulseBotReady);
+    loadCommands("./commands");
+});
+
+client.on("messageCreate", (message) => {
+    if (!message.content.startsWith(process.env.PREFIX) || message.author.bot)
+        return;
+
+    const args = message.content
+        .slice(process.env.PREFIX.length)
+        .trim()
+        .split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    const command = client.commands.get(commandName);
+
+    if (!command) return;
+
+    try {
+        command.execute(message, args, client, pendingBids);
+    } catch (error) {
+        console.error(error);
+        message.reply("There was an error executing that command!");
+    }
+});
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
