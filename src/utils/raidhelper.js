9dfa82896e9d91e6479b3581a3126a543a9c2fbb
mst1987 <marc.studzinski@web.de>
2024-10-03 16:59:33 +0200

    refactor v2

--- src/utils/raidhelper.js
+++ src/utils/raidhelper.js
@@ -3,29 +3,44 @@ const { getCategoryEvents, getCharacterIcon, delay } = require("./helper");
 
 async function getAllSignUps(interaction, categoryId) {
     var categoryEvents = await getCategoryEvents(interaction, categoryId);
-
+    console.log("categoryEvents", categoryEvents);
     const noSignUps = getEventsWithoutSignup(categoryEvents, interaction);
     const signUps = getEventsWithSignup(categoryEvents, interaction);
-
+    console.log("noSignUps", noSignUps);
     const response = {
-        noSignUps: noSignUps.map(channel => `<#${channel.channelId}>`).join(`\n`),
-        signUps: getSignUpsWithSpecs(signUps, interaction)
-    }
+        noSignUps: noSignUps.map((channel) => `<#${channel.channelId}>`).join(`\n`),
+        signUps: getSignUpsWithSpecs(signUps, interaction),
+    };
     return response;
 }
 
 function getEventsWithoutSignup(events, interaction) {
-    return events.filter(event => !event.signUps.find((signup) => signup.userId === interaction.user.id && signup.specName !== 'Absence'));
+    return events.filter(
+        (event) =>
+        !event.signUps.find(
+            (signup) =>
+            signup.userId === interaction.user.id && signup.specName !== "Absence"
+        )
+    );
 }
 
 function getEventsWithSignup(events, interaction) {
-    return events.filter(event => event.signUps.find((signup) => signup.userId === interaction.user.id && signup.specName !== 'Absence'));
+    return events.filter((event) =>
+        event.signUps.find(
+            (signup) =>
+            signup.userId === interaction.user.id && signup.specName !== "Absence"
+        )
+    );
 }
 
 function getSignUpsWithSpecs(events, interaction) {
-    const signUpsWithSpecs = events.map(event => {
-        const matchingSignUps = event.signUps.filter(signUp => signUp.userId === interaction.user.id);
-        const matchingSpecs = matchingSignUps.map(signUp => `${ getCharacterIcon(interaction, signUp.specName) }`).join('');
+    const signUpsWithSpecs = events.map((event) => {
+        const matchingSignUps = event.signUps.filter(
+            (signUp) => signUp.userId === interaction.user.id
+        );
+        const matchingSpecs = matchingSignUps
+            .map((signUp) => `${getCharacterIcon(interaction, signUp.specName)}`)
+            .join("");
 
         return {
             specs: matchingSpecs,
@@ -33,7 +48,9 @@ function getSignUpsWithSpecs(events, interaction) {
         };
     });
 
-    return signUpsWithSpecs.map(channelId => `<#${channelId.channelId}>  ${channelId.specs}\n`).join(`\n`);
+    return signUpsWithSpecs
+        .map((channelId) => `<#${channelId.channelId}>  ${channelId.specs}\n`)
+        .join(`\n`);
 }
 
 async function getCategorySetups(interaction, categoryId) {
@@ -41,19 +58,29 @@ async function getCategorySetups(interaction, categoryId) {
     const raidhelper = new Raidhelper();
     var categoryEvents = await getCategoryEvents(interaction, categoryId);
     if (categoryEvents) {
-        await Promise.all(categoryEvents.map(async(event) => {
-            const setup = await raidhelper.getSetup(event.id);
+        await Promise.all(
+            categoryEvents.map(async(event) => {
+                const setup = await raidhelper.getSetup(event.id);
 
-            if (setup) {
-                events.push({ channelid: event.channelId, startTime: event.startTime, ...setup });
-            } else {
-                events.push({ channelid: event.channelId, startTime: event.startTime });
-            }
-        }));
+                if (setup) {
+                    events.push({
+                        channelid: event.channelId,
+                        startTime: event.startTime,
+                        ...setup,
+                    });
+                } else {
+                    events.push({
+                        channelid: event.channelId,
+                        startTime: event.startTime,
+                    });
+                }
+            })
+        );
 
         events = events.filter((event, index) => {
             if (!event.setup) return event;
-            else return event.setup.some(user => user.userid === interaction.user.id)
+            else
+                return event.setup.some((user) => user.userid === interaction.user.id);
         });
     }
     return events;
@@ -62,22 +89,27 @@ async function getCategorySetups(interaction, categoryId) {
 async function getSetupsFromEvents(client, interaction, events) {
     let myevents = [];
     const raidhelper = new Raidhelper();
-    await Promise.all(events.map(async(event) => {
-        const setup = await raidhelper.getSetup(event.id);
-        if (setup) {
-            myevents.push({ channelid: event.channelId, startTime: event.startTime, ...setup });
-        }
-    }));
+    await Promise.all(
+        events.map(async(event) => {
+            const setup = await raidhelper.getSetup(event.id);
+            if (setup) {
+                myevents.push({
+                    channelid: event.channelId,
+                    startTime: event.startTime,
+                    ...setup,
+                });
+            }
+        })
+    );
 
     myevents = myevents.filter((event, index) => {
-        return event.setup.some(user => user.userid === interaction.user.id)
+        return event.setup.some((user) => user.userid === interaction.user.id);
     });
     return myevents;
 }
 
-
 module.exports = {
     getAllSignUps,
     getCategorySetups,
-    getSetupsFromEvents
-}
+    getSetupsFromEvents,
+};
