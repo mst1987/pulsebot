5114ac237597a5f85ad6d8b779e984dbd2f017a6
mst1987 <marc.studzinski@web.de>
2025-12-17 17:53:40 +0100

    TESTCASE

--- src/utils/responses.js
+++ src/utils/responses.js
@@ -19,6 +19,7 @@ function setupResponse(interaction, event) {
     );
 
   let spec;
+  console.log(spec, emoji);
   if (inSetup) spec = inSetup.spec;
   return `<#${event.channelid}> <t:${Math.round(
     Number(event.startTime)
