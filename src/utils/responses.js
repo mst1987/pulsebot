7a3810435a76d98c9f274d4af06016b87a3c6104
mst1987 <marc.studzinski@web.de>
2026-02-25 00:54:33 +0100

    Setup fix

--- src/utils/responses.js
+++ src/utils/responses.js
@@ -17,7 +17,7 @@ function setupResponse(interaction, event) {
   console.log(inSetup);
 
   let spec;
-  if (typeof inSetup !== "undefined") spec = inSetup.spec;
+  if (inSetup !== undefined) spec = inSetup.spec;
   return `<#${event.channelid}> <t:${Math.round(
     Number(event.startTime)
   )}:R> \n ${
