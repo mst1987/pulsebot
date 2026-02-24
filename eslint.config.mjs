925c640ae8279ecaf97930e0d6d8b9bffd8998bd
mst1987 <marc.studzinski@web.de>
2026-02-25 00:19:21 +0100

    Setup fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,28 @@
-export default [{
+export default [
+  {
     files: ["src/**/*.js"],
     languageOptions: {
-        ecmaVersion: 2022,
-        sourceType: "module",
-        globals: {
-            console: "readonly",
-            require: "readonly",
-            module: "readonly",
-            __dirname: "readonly",
-            process: "readonly",
-            setTimeout: "readonly",
-            setInterval: "readonly",
-        },
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        console: "readonly",
+        require: "readonly",
+        module: "readonly",
+        __dirname: "readonly",
+        process: "readonly",
+        setTimeout: "readonly",
+        setInterval: "readonly",
+      },
     },
     rules: {
-        "no-unused-vars": "warn",
-        "no-undef": "off",
-        "no-console": "off",
-        "consistent-return": "off",
-        eqeqeq: ["error", "always"],
-        indent: ["error", 4],
-        "linebreak-style": ["error", "windows"],
-        quotes: ["error", "double"],
-        semi: ["error", "always"],
+      "no-unused-vars": "warn",
+      "no-undef": "off",
+      "no-console": "off",
+      "consistent-return": "off",
+      eqeqeq: ["error", "always"],
+      "linebreak-style": ["error", "windows"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
     },
-}, ];
+  },
+];
