aa4a275dc12b966df04ba9a4835067f28d3d2653
mst1987 <marc.studzinski@web.de>
2026-02-25 01:00:58 +0100

    Setup fix

--- src/classes/raidhelper.js
+++ src/classes/raidhelper.js
@@ -73,7 +73,7 @@ class Raidhelper {
                 .filter((event) =>
                   event.signUps.find(
                     (signup) =>
-                      signup.id === userid && signup.specName !== "Absence"
+                      signup.userId === userid && signup.specName !== "Absence"
                   )
                 );
             }
@@ -107,7 +107,7 @@ class Raidhelper {
                 (event) =>
                   !event.signUps.find(
                     (signup) =>
-                      signup.id === userid && signup.specName !== "Absence"
+                      signup.userId === userid && signup.specName !== "Absence"
                   )
               );
 
